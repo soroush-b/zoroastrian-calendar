8ade4e2dc60822bc5a16c829bb8a10d70fc555e1
soroush <sori.bk@gmail.com>
2019-12-01 11:13:03 -0800

    Updates

---
+++ precache-manifest.d3574573866cb4fd473acbfc54d2f0f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56863e7a2b5990d8a3f3d1b3fdda4882",
+    "url": "/zoroastrian-calendar/index.html"
+  },
+  {
+    "revision": "523f986f498a0b79501b",
+    "url": "/zoroastrian-calendar/static/css/main.99f7c874.chunk.css"
+  },
+  {
+    "revision": "f370f4430c5ca84eb1ff",
+    "url": "/zoroastrian-calendar/static/js/2.91a11a9e.chunk.js"
+  },
+  {
+    "revision": "523f986f498a0b79501b",
+    "url": "/zoroastrian-calendar/static/js/main.7ff737f4.chunk.js"
+  },
+  {
+    "revision": "07756d18e0870d3cafe0",
+    "url": "/zoroastrian-calendar/static/js/runtime-main.1abbb96a.js"
+  }
+]);
